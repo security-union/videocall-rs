0bcabb625782824ea774b41fe6fca32b1cf170c9
Dario Lencina <dario@securityunion.dev>
2022-10-09 16:32:58 -0400

    save

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,8 +1,9 @@
-use protobuf;
 use serde::{Deserialize, Serialize};
+pub mod protos;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct HelloResponse {
     pub name: String
 }
+
--- yew-ui/src/main.rs
+++ yew-ui/src/main.rs
@@ -1,10 +1,11 @@
 use js_sys::Array;
 use js_sys::Boolean;
 use js_sys::JsString;
+use js_sys::Number;
 use js_sys::Reflect;
-use reqwasm::http::Request;
-use wasm_bindgen::JsValue;
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::Closure;
+use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::HtmlVideoElement;
 use yew::prelude::*;
@@ -13,6 +14,7 @@ extern crate lazy_static;
 use gloo_console::log;
 use gloo_utils::document;
 use gloo_utils::window;
+use types::protos::media_packet::MediaPacket;
 use wasm_bindgen::JsCast;
 use web_sys::*;
 use yew_router::prelude::*;
@@ -37,7 +39,7 @@ lazy_static! {
 #[derive(Properties, Debug, PartialEq)]
 struct MeetingProps {
     #[prop_or_default]
-    pub id: String
+    pub id: String,
 }
 
 #[derive(Clone, Routable, PartialEq)]
@@ -45,7 +47,7 @@ enum Route {
     #[at("/login")]
     Login,
     #[at("/:id")]
-    Meeting {id: String},
+    Meeting { id: String },
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -110,94 +112,104 @@ fn meeting(props: &MeetingProps) -> Html {
     ));
     let id = props.id.clone();
 
-    use_effect_with_deps( move |_| {
-        wasm_bindgen_futures::spawn_local( async move {
-            let navigator = window().navigator();
-            let media_devices = navigator.media_devices().unwrap();
-            let video_element = window()
-                .document()
-                .unwrap()
-                .get_element_by_id("webcam")
-                .unwrap()
-                .unchecked_into::<HtmlVideoElement>();
-            
-            let mut constraints = MediaStreamConstraints::new();
-            constraints.video(&Boolean::from(true));
-            let devices_query = media_devices
-            .get_user_media_with_constraints(&constraints).unwrap();
-            let device = JsFuture::from(devices_query)
-                .await
-                .unwrap()
-                .unchecked_into::<MediaStream>();
-            video_element.set_src_object(Some(&device));
-            let video_track = Box::new(
-                device.get_video_tracks()
-                .find(&mut |_: JsValue, _:u32, _:Array | true)
-                .unchecked_into::<VideoTrack>()
-            );
+    use_effect_with_deps(
+        move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let navigator = window().navigator();
+                let media_devices = navigator.media_devices().unwrap();
+                let video_element = window()
+                    .document()
+                    .unwrap()
+                    .get_element_by_id("webcam")
+                    .unwrap()
+                    .unchecked_into::<HtmlVideoElement>();
 
-            let error_handler = Closure::wrap(Box::new(move |e:JsValue| {
-                console::log_1(&JsString::from("on errror"));
-                console::log_1(&e);
-            }) as Box<dyn FnMut(JsValue)>);
-
-            let output_handler = Closure::wrap(Box::new(move | chunk: JsValue| {
-                // let video_chunk = chunk.unchecked_into::<EncodedVideoChunk>();
-                // video_context.dispatch(
-                //     EncodedVideoChunkWrapper { chunk: Some(video_chunk)}
-                // );
-            }) as Box<dyn FnMut(JsValue)>);
-            let video_encoder_init = VideoEncoderInit::new(
-                error_handler.as_ref().unchecked_ref(),
-                output_handler.as_ref().unchecked_ref()
-            );
-            let video_encoder = VideoEncoder::new(&video_encoder_init).unwrap();
-            let settings = &mut video_track
-                .clone()
-                .unchecked_into::<MediaStreamTrack>()
-                .get_settings();
-            settings.width(VIDEO_WIDTH);
-            settings.height(VIDEO_HEIGHT);
-            let video_encoder_config = VideoEncoderConfig::new(
-                &VIDEO_CODEC,
-                VIDEO_HEIGHT as u32,
-                VIDEO_WIDTH as u32
-            );
-            video_encoder.configure(&video_encoder_config);
-            let processor = MediaStreamTrackProcessor::new(
-                &MediaStreamTrackProcessorInit::new(
-                    &video_track.unchecked_into::<MediaStreamTrack>(),
-                )
-            ).unwrap();
-            let reader = processor
-                .readable()
-                .get_reader()
-                .unchecked_into::<ReadableStreamDefaultReader>();
-            loop {
-                let result = JsFuture::from(reader.read())
+                let mut constraints = MediaStreamConstraints::new();
+                constraints.video(&Boolean::from(true));
+                let devices_query = media_devices
+                    .get_user_media_with_constraints(&constraints)
+                    .unwrap();
+                let device = JsFuture::from(devices_query)
                     .await
-                    .map_err(|e| {
+                    .unwrap()
+                    .unchecked_into::<MediaStream>();
+                video_element.set_src_object(Some(&device));
+                let video_track = Box::new(
+                    device
+                        .get_video_tracks()
+                        .find(&mut |_: JsValue, _: u32, _: Array| true)
+                        .unchecked_into::<VideoTrack>(),
+                );
+
+                let error_handler = Closure::wrap(Box::new(move |e: JsValue| {
+                    console::log_1(&JsString::from("on errror"));
+                    console::log_1(&e);
+                }) as Box<dyn FnMut(JsValue)>);
+
+                let output_handler = Closure::wrap(Box::new(move |chunk: JsValue| {
+                    let chunk = web_sys::EncodedVideoChunk::from(chunk);
+                    let mut media_packet = MediaPacket::default();
+                    media_packet.email = "dario".to_string();
+                    let byte_length: Number = Reflect::get(&chunk, &JsString::from("byteLength"))
+                        .unwrap()
+                        .into();
+                    let byte_length: usize = byte_length.as_f64().unwrap() as usize;
+                    let mut chunk_data: Vec<u8> = vec![0; byte_length];
+                    let mut chunk_data = chunk_data.as_mut_slice();
+                    chunk.copy_to_with_u8_array(&mut chunk_data);
+                    media_packet.video = chunk_data.to_vec();
+
+                    // let video_chunk = chunk.unchecked_into::<EncodedVideoChunk>();
+                    // video_context.dispatch(
+                    //     EncodedVideoChunkWrapper { chunk: Some(video_chunk)}
+                    // );
+                }) as Box<dyn FnMut(JsValue)>);
+                let video_encoder_init = VideoEncoderInit::new(
+                    error_handler.as_ref().unchecked_ref(),
+                    output_handler.as_ref().unchecked_ref(),
+                );
+                let video_encoder = VideoEncoder::new(&video_encoder_init).unwrap();
+                let settings = &mut video_track
+                    .clone()
+                    .unchecked_into::<MediaStreamTrack>()
+                    .get_settings();
+                settings.width(VIDEO_WIDTH);
+                settings.height(VIDEO_HEIGHT);
+                let video_encoder_config =
+                    VideoEncoderConfig::new(&VIDEO_CODEC, VIDEO_HEIGHT as u32, VIDEO_WIDTH as u32);
+                video_encoder.configure(&video_encoder_config);
+                let processor =
+                    MediaStreamTrackProcessor::new(&MediaStreamTrackProcessorInit::new(
+                        &video_track.unchecked_into::<MediaStreamTrack>(),
+                    ))
+                    .unwrap();
+                let reader = processor
+                    .readable()
+                    .get_reader()
+                    .unchecked_into::<ReadableStreamDefaultReader>();
+                loop {
+                    let result = JsFuture::from(reader.read()).await.map_err(|e| {
                         console::log_1(&e);
                     });
-                match result {
-                    Ok(js_frame) => {
-                        let video_frame = 
-                        Reflect::get(&js_frame, &JsString::from("value"))
-                        .unwrap()
-                        .unchecked_into::<VideoFrame>();
-                        video_encoder.encode(&video_frame);
-                        video_frame.close();
-                    }
-                    Err(_e) => {
-                        console::log_1(&JsString::from("error"));
+                    match result {
+                        Ok(js_frame) => {
+                            let video_frame = Reflect::get(&js_frame, &JsString::from("value"))
+                                .unwrap()
+                                .unchecked_into::<VideoFrame>();
+                            video_encoder.encode(&video_frame);
+                            video_frame.close();
+                        }
+                        Err(_e) => {
+                            console::log_1(&JsString::from("error"));
+                        }
                     }
                 }
-            }
-        });
-    || ()
-    }, (),
+            });
+            || ()
+        },
+        (),
     );
-        
+
     html!(
         <div class="producer">
             <h3>{"You"}</h3>

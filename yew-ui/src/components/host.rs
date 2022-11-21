772eb78183e7d79fce42fcf0f9ed20b65b79af37
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2022-11-20 19:50:34 -0500

    MVP of screen share. (#16)

--- yew-ui/src/components/host.rs
+++ yew-ui/src/components/host.rs
@@ -24,14 +24,17 @@ use crate::constants::AUDIO_SAMPLE_RATE;
 use crate::constants::VIDEO_CODEC;
 use crate::constants::VIDEO_HEIGHT;
 use crate::constants::VIDEO_WIDTH;
+use crate::model::transform_screen_chunk;
 use crate::model::transform_video_chunk;
 
 pub enum Msg {
     Start,
+    EnableScreenShare,
 }
 
 pub struct Host {
     pub destroy: Arc<AtomicBool>,
+    pub share_screen: Arc<AtomicBool>,
 }
 
 #[derive(Properties, Debug, PartialEq)]
@@ -47,6 +50,9 @@ pub struct MeetingProps {
 
     #[prop_or_default]
     pub email: String,
+
+    #[prop_or_default]
+    pub share_screen: bool,
 }
 
 impl Component for Host {
@@ -56,10 +62,19 @@ impl Component for Host {
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
             destroy: Arc::new(AtomicBool::new(false)),
+            share_screen: Arc::new(AtomicBool::new(false)),
         }
     }
 
     fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        // Determine if we should start/stop screen share.
+        if ctx.props().share_screen != self.share_screen.load(Ordering::Acquire) {
+            self.share_screen
+                .store(ctx.props().share_screen, Ordering::Release);
+            if ctx.props().share_screen {
+                ctx.link().send_message(Msg::EnableScreenShare);
+            }
+        }
         if first_render {
             ctx.link().send_message(Msg::Start);
         }
@@ -67,6 +82,105 @@ impl Component for Host {
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
+            Msg::EnableScreenShare => {
+                let share_screen = self.share_screen.clone();
+                let on_frame = Box::new(ctx.props().on_frame.clone());
+                let email = Box::new(ctx.props().email.clone());
+                let destroy = self.destroy.clone();
+                let screen_output_handler = {
+                    let email = email.clone();
+                    let on_frame = on_frame.clone();
+                    let mut buffer: [u8; 100000] = [0; 100000];
+                    Box::new(move |chunk: JsValue| {
+                        let chunk = web_sys::EncodedVideoChunk::from(chunk);
+                        let media_packet: MediaPacket =
+                            transform_screen_chunk(chunk, &mut buffer, email.clone());
+                        on_frame.emit(media_packet);
+                    })
+                };
+                wasm_bindgen_futures::spawn_local(async move {
+                    let navigator = window().navigator();
+                    let media_devices = navigator.media_devices().unwrap();
+                    let screen_to_share: MediaStream =
+                        JsFuture::from(media_devices.get_display_media().unwrap())
+                            .await
+                            .unwrap()
+                            .unchecked_into::<MediaStream>();
+
+                    let screen_track = Box::new(
+                        screen_to_share
+                            .get_video_tracks()
+                            .find(&mut |_: JsValue, _: u32, _: Array| true)
+                            .unchecked_into::<VideoTrack>(),
+                    );
+
+                    let screen_error_handler = Closure::wrap(Box::new(move |e: JsValue| {
+                        log!("error_handler error", e);
+                    })
+                        as Box<dyn FnMut(JsValue)>);
+
+                    let screen_output_handler =
+                        Closure::wrap(screen_output_handler as Box<dyn FnMut(JsValue)>);
+
+                    let screen_encoder_init = VideoEncoderInit::new(
+                        screen_error_handler.as_ref().unchecked_ref(),
+                        screen_output_handler.as_ref().unchecked_ref(),
+                    );
+
+                    let screen_encoder = Box::new(VideoEncoder::new(&screen_encoder_init).unwrap());
+                    let mut screen_encoder_config = VideoEncoderConfig::new(
+                        &VIDEO_CODEC,
+                        VIDEO_HEIGHT as u32,
+                        VIDEO_WIDTH as u32,
+                    );
+                    screen_encoder_config.bitrate(100_000f64);
+                    screen_encoder_config.latency_mode(LatencyMode::Realtime);
+                    screen_encoder.configure(&screen_encoder_config);
+
+                    let screen_processor =
+                        MediaStreamTrackProcessor::new(&MediaStreamTrackProcessorInit::new(
+                            &screen_track.unchecked_into::<MediaStreamTrack>(),
+                        ))
+                        .unwrap();
+
+                    let screen_reader = screen_processor
+                        .readable()
+                        .get_reader()
+                        .unchecked_into::<ReadableStreamDefaultReader>();
+
+                    let mut screen_frame_counter = 0;
+
+                    let poll_screen = async {
+                        loop {
+                            if destroy.load(Ordering::Acquire) {
+                                return;
+                            }
+                            if !share_screen.load(Ordering::Acquire) {
+                                return;
+                            }
+                            match JsFuture::from(screen_reader.read()).await {
+                                Ok(js_frame) => {
+                                    log!("");
+                                    let video_frame =
+                                        Reflect::get(&js_frame, &JsString::from("value"))
+                                            .unwrap()
+                                            .unchecked_into::<VideoFrame>();
+                                    let mut opts = VideoEncoderEncodeOptions::new();
+                                    screen_frame_counter = (screen_frame_counter + 1) % 50;
+                                    opts.key_frame(screen_frame_counter == 0);
+                                    screen_encoder.encode_with_options(&video_frame, &opts);
+                                    video_frame.close();
+                                }
+                                Err(e) => {
+                                    log!("error", e);
+                                }
+                            }
+                        }
+                    };
+                    poll_screen.await;
+                });
+                true
+            }
             Msg::Start => {
                 // 1. Query the first device with a camera and a mic attached.
                 // 2. setup WebCodecs, in particular
@@ -74,10 +188,11 @@ impl Component for Host {
                 let on_frame = Box::new(ctx.props().on_frame.clone());
                 let on_audio = Box::new(ctx.props().on_audio.clone());
                 let email = Box::new(ctx.props().email.clone());
+
                 let video_output_handler = {
                     let email = email.clone();
                     let on_frame = on_frame.clone();
-                    let mut buffer: [u8; 300000] = [0; 300000];
+                    let mut buffer: [u8; 100000] = [0; 100000];
                     Box::new(move |chunk: JsValue| {
                         let chunk = web_sys::EncodedVideoChunk::from(chunk);
                         let media_packet: MediaPacket =
@@ -89,7 +204,9 @@ impl Component for Host {
                 let destroy = self.destroy.clone();
                 wasm_bindgen_futures::spawn_local(async move {
                     let navigator = window().navigator();
+
                     let media_devices = navigator.media_devices().unwrap();
+
                     let video_element = window()
                         .document()
                         .unwrap()
@@ -138,6 +255,7 @@ impl Component for Host {
                     );
 
                     let video_encoder = Box::new(VideoEncoder::new(&video_encoder_init).unwrap());
+
                     let settings = &mut video_track
                         .clone()
                         .unchecked_into::<MediaStreamTrack>()
@@ -182,7 +300,7 @@ impl Component for Host {
                         .readable()
                         .get_reader()
                         .unchecked_into::<ReadableStreamDefaultReader>();
-                    let mut counter = 0;
+                    let mut video_frame_counter = 0;
                     let poll_video = async {
                         loop {
                             if destroy.load(Ordering::Acquire) {
@@ -195,8 +313,8 @@ impl Component for Host {
                                             .unwrap()
                                             .unchecked_into::<VideoFrame>();
                                     let mut opts = VideoEncoderEncodeOptions::new();
-                                    counter = (counter + 1) % 50;
-                                    opts.key_frame(counter == 0);
+                                    video_frame_counter = (video_frame_counter + 1) % 50;
+                                    opts.key_frame(video_frame_counter == 0);
                                     video_encoder.encode_with_options(&video_frame, &opts);
                                     video_frame.close();
                                 }
@@ -226,7 +344,9 @@ impl Component for Host {
                             }
                         }
                     };
+
                     join!(poll_video, poll_audio).await;
+                    log!("Killing streamer");
                 });
                 true
             }
@@ -240,6 +360,7 @@ impl Component for Host {
     }
 
     fn destroy(&mut self, _ctx: &Context<Self>) {
+        log!("destroying");
         self.destroy.store(true, Ordering::Release);
     }
 }

d8a0d29fdb6ca460c2661677febb4e59e6f9dce0
leone <leo.vernisse@gmail.com>
2023-06-12 16:37:52 +0200

    feature: tailwind css (#53)
    * feature: tailwind css

--- yew-ui/src/pages/home.rs
+++ yew-ui/src/pages/home.rs
@@ -25,15 +25,17 @@ pub fn home () -> Html {
         })
     };
     html! {
-        <div class="home-page">
-            <div>
-                <h1>{ "Welcome on zoom-rs !" }</h1>
-                <p>{ "This is a web app to manage your zoom meetings." }</p>
+        <div class="flex justify-center items-center content-center flex-col w-full">
+            <div class="flex items-center flex-col">
+                <h1 class="text-xl">{ "Welcome on zoom-rs !" }</h1>
+                <p class="text-xs">{ "This is a web app to manage your zoom meetings." }</p>
             </div>
             <form {onsubmit}>
-                <input label="username" type="text" placeholder="Username" ref={username_ref}  />
-                <input label="meeting_id" type="text" placeholder="Meeting ID" ref={meeting_id_ref} />
-                <input type="submit" value="Join" />
+                <div class="py-4">
+                    <input class="rounded-md mr-1 p-2 text-black" label="username" type="text" placeholder="Username" ref={username_ref}  />
+                    <input class="rounded-md ml-1 p-2 text-black" label="meeting_id" type="text" placeholder="Meeting ID" ref={meeting_id_ref} />
+                </div>
+                <input type="submit" value="JOIN" class="py-2 px-4 pointer bg-yew-blue rounded-md w-full cursor-pointer" />
             </form>
         </div>
     }

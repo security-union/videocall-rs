d023b52898aaec631fb42ebac8abdb15f910a2af
Griffin Obeid <griffobeid@gmail.com>
2023-06-14 17:11:48 -0400

    Using nats to allow horizontal scaling

--- actix-api/src/actors/chat_server.rs
+++ actix-api/src/actors/chat_server.rs
@@ -4,12 +4,9 @@ use crate::messages::{
 };
 
 use actix::{Actor, Context, Handler, MessageResult, Recipient};
-use log::{debug, info};
+use log::{debug, info, trace};
 use protobuf::Message as ProtobufMessage;
-use std::{
-    collections::HashMap,
-    sync::Arc,
-};
+use std::{collections::HashMap, sync::Arc};
 use types::protos::media_packet::MediaPacket;
 
 use super::chat_session::{RoomId, SessionId};
@@ -22,7 +19,10 @@ pub struct ChatServer {
 
 impl ChatServer {
     pub fn new() -> Self {
-        let nc = nats::Options::new().with_name("actix-api").connect(std::env::var("NATS_URL").expect("NATS_URL env var must be defined")).unwrap();
+        let nc = nats::Options::new()
+            .with_name("actix-api")
+            .connect(std::env::var("NATS_URL").expect("NATS_URL env var must be defined"))
+            .unwrap();
         ChatServer {
             nc,
             active_subs: HashMap::new(),
@@ -30,15 +30,11 @@ impl ChatServer {
         }
     }
 
-    pub fn send_message(
-        &self,
-        room: &RoomId,
-        message: Arc<MediaPacket>,
-        skip_id: &String,
-        user: String,
-    ) {
-        let subject = format!("room.{}.{}", room, user);
-        self.nc.publish(&subject, message.write_to_bytes().unwrap()).unwrap();
+    pub fn send_message(&self, room: &RoomId, message: Arc<MediaPacket>, session_id: SessionId) {
+        let subject = format!("room.{}.{}", room, session_id);
+        self.nc
+            .publish(&subject, message.write_to_bytes().unwrap())
+            .unwrap();
     }
 
     pub fn leave_rooms(&mut self, session_id: &SessionId) {
@@ -88,12 +84,12 @@ impl Handler<ClientMessage> for ChatServer {
     fn handle(&mut self, msg: ClientMessage, _ctx: &mut Self::Context) -> Self::Result {
         let ClientMessage {
             session,
-            user,
             room,
             msg,
+            user: _,
         } = msg;
-        debug!("got message in server room {} session {}", room, session);
-        self.send_message(&room, msg.media_packet, &session, user);
+        trace!("got message in server room {} session {}", room, session);
+        self.send_message(&room, msg.media_packet, session);
     }
 }
 
@@ -109,23 +105,37 @@ impl Handler<JoinRoom> for ChatServer {
         let subject = format!("room.{}.*", room);
         let queue = format!("{}-{}", session, room);
         let session_recipient = self.sessions.get(&session).unwrap().clone();
+        let room_clone = room.clone();
         let session_clone = session.clone();
-        let sub = self.nc.queue_subscribe(&subject, &queue).unwrap().with_handler(move |msg| {
-            let msg = Message {
-                nickname: Arc::new(None),
-                msg: Arc::new(MediaPacket::parse_from_bytes(&msg.data).unwrap()),
-            };
-            match session_recipient.try_send(msg) {
-                Ok(_) => Ok(()),
-                Err(e) => {
-                    log::error!("error sending message to session: {}", e);
-                    Ok(())
+        let sub = self
+            .nc
+            .queue_subscribe(&subject, &queue)
+            .unwrap()
+            .with_handler(move |msg| {
+                if msg.subject == format!("room.{}.{}", room_clone, session_clone) {
+                    return Ok(());
                 }
-            }
-        });
+                let msg = MediaPacket::parse_from_bytes(&msg.data).unwrap();
+                let msg = Message {
+                    nickname: Arc::new(Some(msg.email.clone())),
+                    msg: Arc::new(msg),
+                };
+
+                match session_recipient.try_send(msg) {
+                    Ok(_) => Ok(()),
+                    Err(e) => {
+                        log::error!("error sending message to session: {}", e);
+                        Ok(())
+                    }
+                }
+            });
         debug!("Subscribed to subject {} with queue {}", subject, queue);
 
-        let result = self.active_subs.insert(session.clone(), sub).map(|_| ()).ok_or("The session is already subscribed".into());
+        let result = self
+            .active_subs
+            .insert(session.clone(), sub)
+            .map(|_| ())
+            .ok_or("The session is already subscribed".into());
         info!(
             "someone connected to room {} with session {} result {:?}",
             room.clone(),

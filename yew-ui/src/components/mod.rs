95cda70ee11974a560d160793521f46c6f4bd3d1
ronen barzel <ronen@barzel.org>
2023-06-18 00:35:21 +0100

    Factor out AudioPeerDecoder and VideoPeerDecoder
    * Created a new submodule `model/decode/`
    
    * Factored screen, video, and audio decoding code out of
      `components/attendants.rs`, into `model/decode/peer_decoder.rs`
    
    * Moved `components/video_decoder_with_buffer.rs` into `model/decode/`
    
    * Moved configure_audio_context() from `model/mod.rs` into
      `model/decode/config.rs`

--- yew-ui/src/components/mod.rs
+++ yew-ui/src/components/mod.rs
@@ -2,4 +2,3 @@ pub mod attendants;
 pub mod device_permissions;
 pub mod device_selector;
 pub mod host;
-pub mod video_decoder_with_buffer;
---
+++ yew-ui/src/model/decode/mod.rs
@@ -0,0 +1,5 @@
+mod config;
+mod video_decoder_with_buffer;
+mod peer_decoder;
+
+pub use peer_decoder::{AudioPeerDecoder, VideoPeerDecoder};

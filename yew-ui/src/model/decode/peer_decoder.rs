f3bd4a5af7c5ab840e49d2fee6773a42047a8f61
ronen barzel <ronen@barzel.org>
2023-06-27 14:18:58 +0100

    Merge changes from #83 : Play packets in order

--- yew-ui/src/model/decode/peer_decoder.rs
+++ yew-ui/src/model/decode/peer_decoder.rs
@@ -38,6 +38,7 @@ use web_sys::{window};
 pub struct PeerDecoder<WebDecoder,Chunk> {
     decoder: WebDecoder,
     waiting_for_keyframe: bool,
+    last_sequence: u64,
     _error: Closure<dyn FnMut(JsValue)>, // member exists to keep the closure in scope for the life of the struct
     _output: Closure<dyn FnMut(Chunk)>, // member exists to keep the closure in scope for the life of the struct
 }
@@ -58,8 +59,11 @@ macro_rules! impl_decode{
             if !$self.waiting_for_keyframe || chunk_type == <$ChunkType>::Key {
                 match $self.decoder.state() {
                     CodecState::Configured => {
-                        $self.decoder.decode(opt_ref!($self.get_chunk($packet, chunk_type), $ref));
-                        $self.waiting_for_keyframe = false;
+                        if $self.last_sequence < $packet.video_metadata.sequence {
+                            $self.decoder.decode(opt_ref!($self.get_chunk($packet, chunk_type), $ref));
+                            $self.waiting_for_keyframe = false;
+                            $self.last_sequence = $packet.video_metadata.sequence;
+                        }
                     }
                     CodecState::Closed => {
                         return Err(());
@@ -126,6 +130,7 @@ impl PeerDecoder<VideoDecoderWithBuffer,JsValue> {
         PeerDecoder {
             decoder,
             waiting_for_keyframe: true,
+            last_sequence: 0,
             _error: error,
             _output: output,
         }
@@ -194,6 +199,7 @@ impl PeerDecoder<AudioDecoder,AudioData> {
         PeerDecoder {
             decoder,
             waiting_for_keyframe: true,
+            last_sequence: 0,
             _error: error,
             _output: output,
         }

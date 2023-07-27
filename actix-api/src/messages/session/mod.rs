20e60278279589ac6fecc934b62e3e3f4f1d7f69
Griffin Obeid <griffobeid@gmail.com>
2023-07-27 09:48:40 -0400

    Remove spots that websocket API looks at packets to prep for e2ee (#109)
    * save
    
    * Reset ui changes
    
    * cargo fmt
    
    * remove unused deps

--- actix-api/src/messages/session/mod.rs
+++ actix-api/src/messages/session/mod.rs
@@ -1,11 +1,7 @@
-use std::sync::Arc;
-
 use actix::Message as ActixMessage;
-use types::protos::media_packet::MediaPacket;
 
 #[derive(ActixMessage)]
 #[rtype(result = "()")]
 pub struct Message {
-    pub nickname: Arc<Option<String>>,
-    pub msg: Arc<MediaPacket>,
+    pub msg: Vec<u8>,
 }

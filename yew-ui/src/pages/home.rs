056bcd3e9b5af47aa96b535e3df29e4afd8577dd
Griffin Obeid <griffobeid@gmail.com>
2023-09-12 09:45:22 -0400

    Form validation (#141)
    * Form validation
    
    * save
    
    * Adding validation on the backend
    
    * Adding validation on the backend
    
    * clippy
    
    * Uncomment code and remove log statement

--- yew-ui/src/pages/home.rs
+++ yew-ui/src/pages/home.rs
@@ -4,6 +4,8 @@ use yew_router::prelude::*;
 
 use crate::Route;
 
+const TEXT_INPUT_CLASSES: &'static str = "rounded-md mx-2 p-2 text-black required:ring-2 required:ring-red-500 required:valid:ring-2 required:valid:ring-green-500";
+
 #[function_component(Home)]
 pub fn home() -> Html {
     let navigator = use_navigator().unwrap();
@@ -29,11 +31,28 @@ pub fn home() -> Html {
             <div class="flex items-center flex-col">
                 <h1 class="text-xl">{ "videocall-rs" }</h1>
                 <p class="text-xs">{ "Create your videocall by creating a user and meeting id." }</p>
+                <p class="text-xs">{ "Characters allowed: a-z, A-Z, 0-9, and _" }</p>
             </div>
             <form {onsubmit}>
                 <div class="py-4">
-                    <input class="rounded-md mr-1 p-2 text-black" label="username" type="text" placeholder="Username" ref={username_ref}  />
-                    <input class="rounded-md ml-1 p-2 text-black" label="meeting_id" type="text" placeholder="Meeting ID" ref={meeting_id_ref} />
+                    <input
+                        class={TEXT_INPUT_CLASSES}
+                        label="username"
+                        type="text"
+                        placeholder="Username"
+                        ref={username_ref}
+                        required={true}
+                        pattern="^[a-zA-Z0-9_]*$"
+                    />
+                    <input
+                        class={TEXT_INPUT_CLASSES}
+                        label="meeting_id"
+                        type="text"
+                        placeholder="Meeting ID"
+                        ref={meeting_id_ref}
+                        required={true}
+                        pattern="^[a-zA-Z0-9_]*$"
+                    />
                 </div>
                 <input type="submit" value="JOIN" class="py-2 px-4 pointer bg-yew-blue rounded-md w-full cursor-pointer" />
             </form>

854aab0598c8cb7105dec48ae4cdf9cd9512307f
ronen barzel <ronen@barzel.org>
2023-11-10 17:47:55 +0000

    launch_chrome.sh: only create certificate if it doesn't already exist

--- launch_chrome.sh
+++ launch_chrome.sh
@@ -3,24 +3,22 @@
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CERTSPATH="$SCRIPTPATH/actix-api/certs"
 
-# generate certificate
+if ! [ -f "$CERTSPATH/localhost.der" ] ; then
+    echo "Generating certificate in $CERTSPATH"
+    openssl req -x509 -newkey rsa:2048 -keyout "$CERTSPATH/localhost.key" -out "$CERTSPATH/localhost.pem" -days 365 -nodes -subj "/CN=127.0.0.1"
+    openssl x509 -in "$CERTSPATH/localhost.pem" -outform der -out "$CERTSPATH/localhost.der"
+    openssl rsa -in "$CERTSPATH/localhost.key" -outform DER -out "$CERTSPATH/localhost_key.der"
+fi
 
-openssl req -x509 -newkey rsa:2048 -keyout $SCRIPTPATH/actix-api/certs/localhost.key -out $SCRIPTPATH/actix-api/certs/localhost.pem -days 365 -nodes -subj "/CN=127.0.0.1"
-
-openssl x509 -in $SCRIPTPATH/actix-api/certs/localhost.pem -outform der -out $SCRIPTPATH/actix-api/certs/localhost.der
-
-openssl rsa -in $SCRIPTPATH/actix-api/certs/localhost.key -outform DER -out $SCRIPTPATH/actix-api/certs/localhost_key.der
-
-SPKI=`openssl x509 -inform der -in $SCRIPTPATH/actix-api/certs/localhost.der -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64`
-
-echo "Got cert key $SPKI"
+SPKI=$(openssl x509 -inform der -in "$CERTSPATH/localhost.der" -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)
 
 echo "Opening google chrome"
 
-case `uname` in
-    (*Linux*)  google-chrome --origin-to-force-quic-on=127.0.0.1:4433 --ignore-certificate-errors-spki-list=$SPKI --enable-logging --v=1 ;;
-    (*Darwin*)  open -a "Google Chrome" --args --origin-to-force-quic-on=127.0.0.1:4433 --ignore-certificate-errors-spki-list=$SPKI --enable-logging --v=1 ;;
+case $(uname) in
+    (*Linux*)  google-chrome --origin-to-force-quic-on=127.0.0.1:4433 --ignore-certificate-errors-spki-list="$SPKI" --enable-logging --v=1 ;;
+    (*Darwin*)  open -a "Google Chrome" --args --origin-to-force-quic-on=127.0.0.1:4433 --ignore-certificate-errors-spki-list="$SPKI" --enable-logging --v=1 ;;
 esac
 
 ## Logs are stored to ~/Library/Application Support/Google/Chrome/chrome_debug.log

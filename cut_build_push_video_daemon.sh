c49fe703f1c3f5fa0271b5d6e5c9a671f06bc3c1
Dario Lencina <dario@securityunion.dev>
2024-12-06 09:02:48 -0500

    save

--- cut_build_push_video_daemon.sh
+++ cut_build_push_video_daemon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-IMAGE_URL=securityunion/video-call-daemon:staging
+IMAGE_URL=securityunion/videocall-daemon:staging
 echo "Building image $IMAGE_URL"
 
-if ! docker build -t $IMAGE_URL . --file Dockerfile.video-call-daemon; then
+if ! docker build -t $IMAGE_URL . --file Dockerfile.videocall-daemon; then
     echo "Failed to build docker image"
 else
     docker push $IMAGE_URL

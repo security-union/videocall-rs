09f537242c463f2dfc45e32f75506dc946fb7271
Dario Lencina <dario@securityunion.dev>
2025-02-09 11:17:18 -0500

    I can see a beautiful clean image on macos, now I need to make it fast

--- videocall-daemon/src/modes/test_camera.rs
+++ videocall-daemon/src/modes/test_camera.rs
@@ -1,5 +1,8 @@
+use std::io::Read;
+
+use image::ImageBuffer;
 use nokhwa::{
-    pixel_format::{BgraFormat},
+    pixel_format::BgraFormat,
     utils::{CameraIndex, RequestedFormat, RequestedFormatType},
     Camera,
 };
@@ -20,6 +23,7 @@ pub fn test_camera(info: TestCamera) {
     )
     .unwrap();
     let actual_format = camera.camera_format();
+    println!("Actual format: {:?}", actual_format);
     camera.open_stream().unwrap();
     // Image dimensions
     let width = actual_format.resolution().width();
@@ -32,10 +36,10 @@ pub fn test_camera(info: TestCamera) {
         .build(&event_loop)
         .unwrap();
 
-    let window_scale = 0.5f64;
+    let window_scale = 1f64;
     let scaled_width = (width as f64 * window_scale) as u32;
     let scaled_height = (height as f64 * window_scale) as u32;
-    let surface_texture = SurfaceTexture::new(scaled_width, scaled_height, &window);
+    let surface_texture = SurfaceTexture::new(width, height, &window);
     let mut pixels: Pixels = Pixels::new(scaled_width, scaled_height, surface_texture).unwrap();
     // print render format
     println!("Render format: {:?}", pixels.render_texture_format());
@@ -45,23 +49,26 @@ pub fn test_camera(info: TestCamera) {
         *control_flow = ControlFlow::Wait;
         match event {
             Event::RedrawRequested(_) => {
+                // On the first frame, we need to resize the window to match the camera resolution
+
                 // Grab a frame from the camera
                 let camera_buffer = camera.frame().unwrap();
-
                 // write to pixels
-                let camera_frame = camera_buffer.buffer().len();
+                // Transform image from NV12 to BGRA
                 let pixels_frame = pixels.get_frame().len();
-
+                let camera_buffer = camera_buffer.decode_image::<BgraFormat>().unwrap();
+                let camera_buffer = camera_buffer.to_vec();
+                let camera_frame = camera_buffer.len();
                 // Write the frame to the pixels buffer, considering the format
                 if camera_frame == pixels_frame {
                     pixels
                         .get_frame_mut()
-                        .copy_from_slice(camera_buffer.buffer());
+                        .copy_from_slice(&camera_buffer);
                 } else {
                     // only copy enough to fill the pixels buffer,
                     pixels
                         .get_frame_mut()
-                        .copy_from_slice(&camera_buffer.buffer()[..pixels_frame]);
+                        .copy_from_slice(&camera_buffer[..pixels_frame]);
 
                     eprintln!(
                         "Frame sizes do not match: camera_frame: {}, pixels_frame: {}",

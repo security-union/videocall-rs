fba5551b5eb2629175f5ad4af6c16e0cfa84fb0e
Dario Lencina <dario@securityunion.dev>
2025-02-27 08:56:15 -0500

    rename to videocall-cli

--- cut_build_push_videocall_cli.sh
+++ cut_build_push_videocall_cli.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-IMAGE_URL=securityunion/videocall-daemon:staging
+IMAGE_URL=securityunion/videocall-cli:staging
 echo "Building image $IMAGE_URL"
 
-if ! docker build -t $IMAGE_URL . --file Dockerfile.videocall-daemon; then
+if ! docker build -t $IMAGE_URL . --file Dockerfile.videocall-cli; then
     echo "Failed to build docker image"
 else
     docker push $IMAGE_URL

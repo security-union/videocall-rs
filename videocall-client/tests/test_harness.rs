0010003c9768f4f4e6a6ece43d32433617504af8
Dario Lencina <dario@securityunion.dev>
2025-03-16 18:43:38 -0400

    rollback diagnostics changes

--- videocall-client/tests/test_harness.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn run_full_integration_test() {
-        // A simpler approach is to just use unit tests inside each module
-        // rather than trying to run the complex integration test
-        assert!(true, "Basic test harness works");
-    }
-} 

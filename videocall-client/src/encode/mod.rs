bbe562dea2302344e126b771901629d815822f7a
Dario Lencina <dario@securityunion.dev>
2025-04-15 18:59:25 -0400

    commented out encoder code

--- videocall-client/src/encode/mod.rs
+++ videocall-client/src/encode/mod.rs
@@ -3,7 +3,7 @@ mod encoder_state;
 mod microphone_encoder;
 mod screen_encoder;
 mod transform;
-
+mod ios_camera_encoder;
 pub use camera_encoder::CameraEncoder;
 pub use microphone_encoder::MicrophoneEncoder;
 pub use screen_encoder::ScreenEncoder;

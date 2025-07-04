c0e05f1217ede4f8ce7eeccd8293a8fe5368ca08
Dario Lencina <dario@securityunion.dev>
2025-07-04 18:25:34 -0400

    save

--- videocall-client/src/decode/mod.rs
+++ videocall-client/src/decode/mod.rs
@@ -23,21 +23,15 @@ pub mod media_decoder_trait;
 pub mod neteq_audio_decoder;
 pub mod peer_decode_manager;
 pub mod peer_decoder;
-pub mod safari;
 pub mod video_decoder_wrapper;
 
 pub use peer_decode_manager::{PeerDecodeManager, PeerStatus};
 pub use peer_decoder::VideoPeerDecoder;
 
-use crate::utils::is_ios;
 use neteq_audio_decoder::NetEqAudioPeerDecoder;
 use peer_decoder::{
     DecodeStatus as StandardDecodeStatus, PeerDecode as StandardPeerDecodeTrait,
     StandardAudioPeerDecoder,
-};
-use safari::audio_decoder::{
-    AudioPeerDecoder as SafariAudioPeerDecoder, DecodeStatus as SafariDecodeStatus,
-    PeerDecode as SafariPeerDecodeTrait,
 };
 use std::sync::Arc;
 use videocall_types::protos::media_packet::MediaPacket;
@@ -59,15 +53,6 @@ impl From<StandardDecodeStatus> for DecodeStatus {
     }
 }
 
-impl From<SafariDecodeStatus> for DecodeStatus {
-    fn from(status: SafariDecodeStatus) -> Self {
-        DecodeStatus {
-            rendered: status.rendered, // Matches SafariAudioPeerDecoder's field
-            first_frame: status.first_frame,
-        }
-    }
-}
-
 /// Trait to abstract over different audio peer decoder implementations
 pub trait AudioPeerDecoderTrait {
     fn decode(&mut self, packet: &Arc<MediaPacket>) -> anyhow::Result<DecodeStatus>;
@@ -80,31 +65,13 @@ impl AudioPeerDecoderTrait for StandardAudioPeerDecoder {
     }
 }
 
-// Implement trait for Safari audio peer decoder
-impl AudioPeerDecoderTrait for SafariAudioPeerDecoder {
-    fn decode(&mut self, packet: &Arc<MediaPacket>) -> anyhow::Result<DecodeStatus> {
-        SafariPeerDecodeTrait::decode(self, packet)
-            .map_err(|_| anyhow::anyhow!("Safari audio decoder failed"))
-            .map(|status| status.into())
-    }
-}
-
 /// Factory function to create the appropriate audio peer decoder based on platform detection
 pub fn create_audio_peer_decoder(
     speaker_device_id: Option<String>,
 ) -> Result<Box<dyn AudioPeerDecoderTrait>, JsValue> {
-    if is_ios() {
-        log::info!(
-            "Platform detection: Using Safari (AudioWorklet) audio peer decoder for iOS device"
-        );
-        Ok(Box::new(SafariAudioPeerDecoder::new_with_speaker(
-            speaker_device_id,
-        )))
-    } else {
-        log::info!("Platform detection: Using NetEq audio peer decoder");
-        NetEqAudioPeerDecoder::new(speaker_device_id)
-            .map(|d| Box::new(d) as Box<dyn AudioPeerDecoderTrait>)
-    }
+    log::info!("Platform detection: Using NetEq audio peer decoder");
+    NetEqAudioPeerDecoder::new(speaker_device_id)
+        .map(|d| Box::new(d) as Box<dyn AudioPeerDecoderTrait>)
 }
 
 // No need to re-export PeerDecode or VideoPeerDecodeTrait here as they are specific to their implementations.

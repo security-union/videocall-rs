16d0a66054347069a0e9354ffd0695b079c9fe7c
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2025-08-15 00:59:20 -0400

    Add low end android device profiles (#394)
    * preallocate array
    
    * pcmPlayerWorker gone
    
    * save
    
    * save
    
    * save

--- yew-ui/scripts/config.js
+++ yew-ui/scripts/config.js
@@ -1,6 +1,6 @@
 window.__APP_CONFIG = Object.freeze({
   apiBaseUrl: "http://localhost:8080",
-  wsUrl: "ws://localhost:8080",
+  wsUrl: "wss://websocket-us-east.webtransport.video,wss://websocket-singapore.webtransport.video",
   webTransportHost: "https://127.0.0.1:4433",
   oauthEnabled: "false",
   e2eeEnabled: "false",

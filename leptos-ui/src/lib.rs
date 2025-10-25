3ca668056b2ea19c40899e75894d80569e158ac6
Dario Lencina <dario@securityunion.dev>
2025-10-24 23:55:41 -0400

    save

---
+++ leptos-ui/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod app;
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    console_error_panic_hook::set_once();
+    leptos::mount::hydrate_body(App);
+}

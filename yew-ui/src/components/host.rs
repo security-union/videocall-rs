341ea04e232d0ea5584ff1ee9b3a3468f73bc6e9
Antonio Estrada <antonio.estrada@hcl-software.com>
2025-10-28 09:57:49 -0400

    remove change host name from host panel

--- yew-ui/src/components/host.rs
+++ yew-ui/src/components/host.rs
@@ -477,10 +477,10 @@ impl Component for Host {
                         html! {
                             <div class="host-video-wrapper" style="position:relative;">
                                 // <video class="self-camera" autoplay=true id={VIDEO_ELEMENT_ID} playsinline={true} controls={false}></video>
-                                <button class="change-name-fab" title="Change name"
-                                    onclick={ctx.link().callback(|_| Msg::ToggleChangeNameModal)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20h9"/><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4Z"/></svg>
-                                </button>
+                                // <button class="change-name-fab" title="Change name"
+                                //     onclick={ctx.link().callback(|_| Msg::ToggleChangeNameModal)}>
+                                //     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20h9"/><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4Z"/></svg>
+                                // </button>
                             </div>
                         }
                     } else {

942449ee89df87e92fa41132a2433d17a098983b
Dario Lencina <dario@securityunion.dev>
2025-10-29 22:22:23 -0400

    revert config.js changes

--- yew-ui/scripts/config.js
+++ yew-ui/scripts/config.js
@@ -1,13 +1,13 @@
 window.__APP_CONFIG = Object.freeze({
   apiBaseUrl: "http://localhost:8080",
-  wsUrl: "wss://websocket-us-east.webtransport.video,wss://websocket-singapore.webtransport.video",
-  webTransportHost: "https://webtransport-us-east.webtransport.video,https://webtransport-singapore.webtransport.video",
+  wsUrl: "ws://localhost:8080",
+  webTransportHost: "https://127.0.0.1:4433",
   oauthEnabled: "false",
   e2eeEnabled: "false",
   webTransportEnabled: "true",
   usersAllowedToStream: "",
   serverElectionPeriodMs: 2000,
   audioBitrateKbps: 65,
-  videoBitrateKbps: 100,
-  screenBitrateKbps: 100
+  videoBitrateKbps: 1000,
+  screenBitrateKbps: 1000
 });

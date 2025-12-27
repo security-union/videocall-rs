a76c78d0c3c9ecabc771e4f61cd60ba464c81cff
Dario Lencina <dario@securityunion.dev>
2025-12-26 20:01:55 -0500

    fixed camera icon

--- yew-ui/src/components/attendants.rs
+++ yew-ui/src/components/attendants.rs
@@ -913,7 +913,7 @@ impl Component for AttendantsComponent {
                                                         html! {
                                                             <>
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                                    <path d="M16 16v1a4 4 0 0 1-4 4H3a4 4 0 0 1-4-4V7a4 4 0 0 1 4-4h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10"></path>
+                                                                    <path d="M16 16v1a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10"></path>
                                                                     <line x1="1" y1="1" x2="23" y2="23"></line>
                                                                 </svg>
                                                                 <span class="tooltip">{ "Start Video" }</span>

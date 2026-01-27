0e5f53772f7b4e2ecc9062b4448a903b58b5a77c
Alena Melnikava <Alena.Melnikava@hcl-software.com>
2026-01-27 11:00:12 +0200

    Fix problem if media device is not accessible

--- yew-ui/src/components/attendants.rs
+++ yew-ui/src/components/attendants.rs
@@ -455,7 +455,7 @@ impl Component for AttendantsComponent {
                     true
                 }
                 WsAction::RequestMediaPermissions => {
-                    self.media_device_access.request();
+                    self.media_device_access.request(self.mic_enabled, self.video_enabled);
                     false
                 }
                 WsAction::MediaPermissionsGranted => {

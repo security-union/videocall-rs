d53e620c62e7d549c3c40a35c52d18f11820ce61
Antonio Estrada <antonio.estrada@hcl-software.com>
2026-02-17 18:46:05 -0500

    Enable webtransport

--- dioxus-ui/scripts/config.js
+++ dioxus-ui/scripts/config.js
@@ -4,7 +4,7 @@ window.__APP_CONFIG = Object.freeze({
   webTransportHost: "https://127.0.0.1:4433",
   oauthEnabled: "true",
   e2eeEnabled: "false",
-  webTransportEnabled: "false",
+  webTransportEnabled: "true",
   firefoxEnabled: "false",
   usersAllowedToStream: "",
   serverElectionPeriodMs: 2000,

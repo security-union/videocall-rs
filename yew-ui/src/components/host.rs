ea586e94e2d57871be0388662ee94dff9ba45f69
ilya <ilya.tarmanau@hcl-software.com>
2026-02-24 21:38:16 +0100

    added highlighting host's canvas

--- yew-ui/src/components/host.rs
+++ yew-ui/src/components/host.rs
@@ -520,7 +520,7 @@ impl Component for Host {
                                             <path d="M16 16v1a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10"></path>
                                             <line x1="1" y1="1" x2="23" y2="23"></line>
                                         </svg>
-                                        <span class="placeholder-text">{"Camera Off 3222 "}</span>
+                                        <span class="placeholder-text">{"Camera Off"}</span>
                                     </div>
                                 }
                             } else {

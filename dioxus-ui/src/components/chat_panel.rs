7c5992d946b3ca7722b8d656cc5123ea40a7725f
Antonio Estrada <antonio.estrada@hcl-software.com>
2026-03-30 19:29:42 -0400

    tweak ux

--- dioxus-ui/src/components/chat_panel.rs
+++ dioxus-ui/src/components/chat_panel.rs
@@ -302,7 +302,19 @@ pub fn ChatPanel(
                 button {
                     class: "close-button",
                     onclick: move |e| on_close.call(e),
-                    "\u{00d7}"
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: "16",
+                        height: "16",
+                        view_box: "0 0 24 24",
+                        fill: "none",
+                        stroke: "currentColor",
+                        stroke_width: "2",
+                        stroke_linecap: "round",
+                        stroke_linejoin: "round",
+                        line { x1: "18", y1: "6", x2: "6", y2: "18" }
+                        line { x1: "6", y1: "6", x2: "18", y2: "18" }
+                    }
                 }
             }
 

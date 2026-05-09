4caaf5c17399c918dadb584708a4297dcd6ebc2d
John McCarthy <john-mccarthy@hcl-software.com>
2026-05-09 09:14:17 -0400

    update config to match helm

--- dioxus-ui/scripts/config.js
+++ dioxus-ui/scripts/config.js
@@ -7,7 +7,7 @@ window.__APP_CONFIG = ({
   meetingApiBaseUrl: "http://localhost:8082",
   wsUrl: "ws://localhost:8080",
   webTransportHost: "https://127.0.0.1:4433",
-  oauthEnabled: "true",
+  oauthEnabled: "false",
   e2eeEnabled: "false",
   webTransportEnabled: "true",
   firefoxEnabled: "false",
